87cb33bc8f8bb36d02ea85d41c2791d0ccddca4a
Martin van Diest <martin.van.diest@intemo.com>
2023-12-05 16:29:01 +0100

    Day 3 WIP

--- day3/map.py
+++ day3/map.py
@@ -7,70 +7,37 @@ class Map(Matrix):
         self.numbers = []
         self.symbols = ['*', '$', '+', '#', '.', '=', '/', '@', '%', '&', '-']
 
-    def calc(self):
-        nums = []
-        for j in range(self.dimj):
-            num = {
-                'value': [],
-                'start': [],
-                'length': 0,
-            }
+    def add_lines(self, lines):
+        for (j, line) in enumerate(lines):
             for i in range(self.dimi):
-                if self.grid[j][i].isnumeric():
-                    num['value'].append(self.grid[j][i])
-                    num['start'] = [j, i] if not num['start'] else num['start']
+                self.grid[j][i] = line[i]
+
+    def calc(self):
+        num = {
+            'val': 0,
+            'raw': [],
+            'start': (-1, -1),
+            'length': 0,
+            'has_neighbour': False
+        }
+        for j in range(self.dimj):
+            for i in range(self.dimi):
+                if self.grid[j][i] not in self.symbols:
+                    num['raw'] += (self.grid[j][i] * pow(10, num['length']))
+                    num['start'] = (j, i)
                     num['length'] += 1
-                    if not self.grid[j][i+1].isnumeric():
-                        nums.append(num)
+                    if i == (self.dimi - 1) or self.grid[j][i + 1] in self.symbols:
+                        self.numbers.append(num)
                         num = {
-                            'value': [],
-                            'start': [],
+                            'val': 0,
+                            'raw': [],
+                            'start': (-1, -1),
                             'length': 0,
+                            'has_neighbour': False
                         }
-        print(nums)
-        self.numbers = nums
 
     def check_number(self, number):
-        result = True
-        for i in range(number['length']):
-            x = number['start'][0] + i
-            y = number['start'][1]
-            if i == 0:
-                # beginning of number
-                print("beginning of number (%d, %d) and i is  %d" % (x, y, i))
-                if y > 0 and self.grid[y-1][x] in self.symbols:
-                    result = result and False
-                elif x > 0:
-                    if self.grid[y][x - 1] in self.symbols:
-                        result = result and False
-                    elif y > 0 and self.grid[y-1][x - 1] in self.symbols:
-                        result = result and False
-                    elif y < self.dimj-1 and self.grid[y+1][x - 1] in self.symbols:
-                        result = result and False
-                elif y < self.dimj-1 and self.grid[y+1][x] in self.symbols:
-                    result = result and False
-            elif i == number['length'] - 1:
-                # end of number
-                print("end of number (%d, %d) and i is  %d" % (x, y, i))
-                if y > 0 and self.grid[y-1][x] in self.symbols:
-                    result = result and False
-                elif x < self.dimi-1:
-                    if self.grid[y][x + 1] in self.symbols:
-                        result = result and False
-                    elif y > 0 and x < self.dimi and self.grid[y-1][x + 1] in self.symbols:
-                        result = result and False
-                    elif y < self.dimj - 1 and x < self.dimi - 1 and self.grid[y + 1][x + 1] in self.symbols:
-                        result = result and False
-                elif y < self.dimj - 1 and self.grid[y + 1][x] in self.symbols:
-                    result = result and False
-            elif 0 < i < number['length'] - 1:
-                # middle of number
-                print("middle of number (%d, %d) and i is  %d" % (x, y, i))
-                if y > 0 and self.grid[y - 1][x] in self.symbols:
-                    result = result and False
-                elif y < self.dimj - 1 and self.grid[y + 1][x] in self.symbols:
-                    result = result and False
-        return result
+        pass
 
     def __str__(self):
         return super().__str__()
