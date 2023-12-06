4700e2c47b4923e6e110d3e41883734d451ad5c7
Martin van Diest <martin.van.diest@intemo.com>
2023-12-06 11:33:21 +0100

    Day 3 Finished part1 & part2

--- day3/day3.py
+++ day3/day3.py
@@ -17,14 +17,33 @@ datalines2 = """467..114..
     
 def part1():
     # Your code for part 1 goes here
-    m1 = Map(len(datalines2), len(datalines2[0]))
-    m1.add_lines(datalines2)
-    print(m1)
-    print(m1.numbers)
-    for number in m1.numbers:
-        print(number, m1.check_number(number))
+    # m1 = Map(len(datalines2), len(datalines2[0]))
+    # m1.add_lines(datalines2)
+    # print(m1)
+    # print("Sum of parts : ", m1.get_part_sum())
+    #
+    # for the big one :
+    m = Map(len(datalines), len(datalines[0]))
+    m.add_lines(datalines)
+    print("Sum of the parts : ", m.get_part_sum())
 
 
 def part2():
     # Your code for part 2 goes here
-    pass
+    #
+    # First the small one :
+    # m1 = Map(len(datalines2), len(datalines2[0]))
+    # m1.add_lines(datalines2)
+    #
+    # now the big one :
+    m1 = Map(len(datalines), len(datalines[0]))
+    m1.add_lines(datalines)
+    print(m1)
+    ratios = m1.gear_ratios()
+    print("Gear ratios : ", ratios)
+    sum_of_products = 0
+    for ratio in ratios:
+        print("Gear ratio : ", ratio)
+        rs = [r for r in ratio]
+        sum_of_products += (rs[0] * rs[1])
+    print("Sum of products : ", sum_of_products)
--- day3/map.py
+++ day3/map.py
@@ -5,14 +5,45 @@ class Map(Matrix):
     def __init__(self, dimj, dimi):
         super().__init__(dimj, dimi)
         self.numbers = []
-        self.symbols = ['*', '$', '+', '#', '.', '=', '/', '@', '%', '&', '-']
+        self.symbols = ['*', '$', '+', '#', '=', '/', '@', '%', '&', '-', '.']
 
     def add_lines(self, lines):
         for (j, line) in enumerate(lines):
             for i in range(self.dimi):
                 self.grid[j][i] = line[i]
         self._calc()
-        #self.check_numbers()
+        self.check_numbers()
+
+    def get_neighbors(self, row: int, col: int):
+        neighbors = []
+        row_max = self.dimj - 1
+        col_max = self.dimi - 1
+        neighbors = []
+        if row > 0:
+            neighbors.append((row - 1, col))
+            if col > 0:
+                neighbors.append((row - 1, col - 1)) if (row - 1, col - 1) not in neighbors else None
+            if col < col_max:
+                neighbors.append((row - 1, col + 1)) if (row - 1, col + 1) not in neighbors else None
+        if row < row_max:
+            neighbors.append((row + 1, col))
+            if col > 0:
+                neighbors.append((row + 1, col - 1)) if (row + 1, col - 1) not in neighbors else None
+            if col < col_max:
+                neighbors.append((row + 1, col + 1)) if (row + 1, col + 1) not in neighbors else None
+        if col > 0:
+            neighbors.append((row, col - 1))
+            if row > 0:
+                neighbors.append((row - 1, col - 1)) if (row - 1, col - 1) not in neighbors else None
+            if row < row_max:
+                neighbors.append((row + 1, col - 1)) if (row + 1, col - 1) not in neighbors else None
+        if col < col_max:
+            neighbors.append((row, col + 1))
+            if row > 0:
+                neighbors.append((row - 1, col + 1)) if (row - 1, col + 1) not in neighbors else None
+            if row < row_max:
+                neighbors.append((row + 1, col + 1)) if (row + 1, col + 1) not in neighbors else None
+        return neighbors
 
     def _calc(self):
         num = {
@@ -26,7 +57,7 @@ class Map(Matrix):
             for i in range(self.dimi):
                 if self.grid[j][i] not in self.symbols:
                     num['raw'].append(self.grid[j][i])
-                    num['start'] = (j, i)
+                    num['start'] = (j, i) if num['start'] == (-1, -1) else num['start']
                     num['length'] += 1
                     if i == (self.dimi - 1) or (self.grid[j][i + 1] in self.symbols):
                         for ind in range(num['length']):
@@ -40,62 +71,56 @@ class Map(Matrix):
                             'has_neighbour': False
                         }
 
+    def is_number(self, row: int, col: int) -> int:
+        for number in self.numbers:
+            coords = [(number['start'][0], number['start'][1] + c) for c in range(number['length'])]
+            if (row, col) in coords:
+                return number['val']
+        return -1
+
     def check_numbers(self):
-        for num in self.numbers:
-            result = True
-            for i in range(num['length']):
-                x = num['start'][0] + i
-                y = num['start'][1]
-                if i == 0:
-                    # start of number
-                    if x > 0:
-                        if y > 0:
-                            result = result and self.grid[y - 1][x] in self.symbols
-                            result = result and self.grid[y - 1][x - 1] in self.symbols
-                            result = result and self.grid[y][x - 1] in self.symbols
-                            result = result and self.grid[y + 1][x - 1] in self.symbols
-                            result = result and self.grid[y + 1][x] in self.symbols
-                        else:
-                            result = result and self.grid[y][x - 1] in self.symbols
-                            result = result and self.grid[y + 1][x - 1] in self.symbols
-                            result = result and self.grid[y + 1][x] in self.symbols
-                    else:
-                        if y > 0:
-                            result = result and self.grid[y - 1][x] in self.symbols
-                            result = result and self.grid[y + 1][x] in self.symbols
-                        else:
-                            result = result and self.grid[y + 1][x] in self.symbols
-                elif i == num['length'] - 1:
-                    # end of number
-                    if x < self.dimi - 1:
-                        if y < self.dimj - 1:
-                            result = result and self.grid[y - 1][x] in self.symbols
-                            result = result and self.grid[y - 1][x + 1] in self.symbols
-                            result = result and self.grid[y][x + 1] in self.symbols
-                            result = result and self.grid[y + 1][x + 1] in self.symbols
-                            result = result and self.grid[y + 1][x] in self.symbols
-                        else:
-                            result = result and self.grid[y][x + 1] in self.symbols
-                            result = result and self.grid[y - 1][x + 1] in self.symbols
-                            result = result and self.grid[y - 1][x] in self.symbols
-                    else:
-                        if y < self.dimj - 1:
-                            result = result and self.grid[y - 1][x] in self.symbols
-                            result = result and self.grid[y + 1][x] in self.symbols
-                        else:
-                            result = result and self.grid[y - 1][x] in self.symbols
-                else:
-                    # middle of number
-                    if y == 0:
-                        result = result and self.grid[y + 1][x] in self.symbols
-                    elif y == self.dimj - 1:
-                        result = result and self.grid[y - 1][x] in self.symbols
-                    else:
-                        print(i, x, y)
-                        result = result and self.grid[y - 1][x] in self.symbols
-                        result = result and self.grid[y + 1][x] in self.symbols
-                num['has_neighbour'] = result
-                result = True
+        good_numbers = []
+        for number in self.numbers:
+            # get the neigbors for each number and put these in a set.
+            # remove the number coords themselves from the set
+            # check the coords if there is a symbol in the set
+            coords = [(number['start'][0], number['start'][1] + c) for c in range(number['length'])]
+            clean_neighbors = set()
+            neighbors = [self.get_neighbors(cds[0], cds[1]) for cds in coords]
+            for neighbor in neighbors:
+                for n in neighbor:
+                    if n not in coords:
+                        clean_neighbors.add(n)
+            for x in clean_neighbors:
+                if self.grid[x[0]][x[1]] in self.symbols and self.grid[x[0]][x[1]] != '.':
+                    number['has_neighbour'] = True
+
+    def get_part_sum(self):
+        total_sum = 0
+        for number in self.numbers:
+            if number['has_neighbour']:
+                total_sum += number['val']
+        return total_sum
+
+    def gear_ratios(self):
+        gear_ratios = []
+        for j in range(self.dimj):
+            for i in range(self.dimi):
+                if self.grid[j][i] == '*':
+                    gears = set()
+                    neighbors = self.get_neighbors(j, i)
+                    for neighbor in neighbors:
+                        # check if a neighbour is a number
+                        # if we have 2 different numbers as neighbours, we have a gear ratio
+                        gear = self.is_number(neighbor[0], neighbor[1])
+                        if gear != -1:
+                            print("add gear ", gear)
+                            gears.add(gear)
+                    print(gears)
+                    if len(gears) == 2:
+                        gear_ratios.append(gears)
+                        print(gears)
+        return gear_ratios
 
     def __str__(self):
         return super().__str__()
