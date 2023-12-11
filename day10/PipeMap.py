9256a9030358e1113d20bd25e90fd04e89449176
Martin van Diest <martin.van.diest@intemo.com>
2023-12-11 15:25:15 +0100

    Made start with day 10

---
+++ day10/PipeMap.py
@@ -0,0 +1,6 @@
+from MyMods.Matrix import Matrix
+
+
+class PipeMap(Matrix):
+    def __init__(self, dimj, dimi):
+        super().__init__(dimj, dimi)
--- day10/day10.py
+++ day10/day10.py
@@ -1,11 +1,21 @@
 from MyMods.ReadDataFile import read_data
-        
+from day10.PipeMap import PipeMap
+
 datalines = read_data("day10/input_day10.txt")
-    
+
+datalines2 = """.....
+.S-7.
+.|.|.
+.L-J.
+.....""".split("\n")
+
+
 def part1():
+    pipe = PipeMap()
     # Your code for part 1 goes here
     pass
-        
+
+
 def part2():
     # Your code for part 2 goes here
     pass
